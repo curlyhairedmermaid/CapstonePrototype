3bf442503f64ab2f5fb95d88eef06b3f9f04291a
Unknown <petroej1@ferris.edu>
2018-09-22 20:50:12 -0400

    Finished HW
    Player Moves on a Nav mash

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
+            }
+        }
+    }
+    
+}
