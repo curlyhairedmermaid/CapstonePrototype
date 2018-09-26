b7a547cd18b6cd39abb097653f7d19ea05b72caa
Unknown <petroej1@ferris.edu>
2018-09-25 21:37:18 -0400

    Highlight material now is used
    Whenever an object with the highligh script on it is moused over, it turns yellow. For now its default resting color is red.

--- Assets/Highligh.cs
+++ Assets/Highligh.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Highligh : MonoBehaviour {
+public class Highligh : MonoBehaviour
+{
+    Material ToHighlight;
+    bool hovering; 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        ToHighlight = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hovering==true)
+        {
+            ToHighlight.color = Color.yellow;
+        }
+        else
+        {
+            
+            ToHighlight.color = Color.red;
+        }
+        hovering = false;
+
+
+    }
+    private void OnMouseOver()
+    {
+        hovering = true;
+    }
 }
