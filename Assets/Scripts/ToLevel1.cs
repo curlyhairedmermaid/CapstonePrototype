54245020520e41dfcba23beb0cfbbb5eb5b60b84
Jessica <petroej1@ferris.edu>
2018-10-04 13:38:10 -0400

    Scene Management

---
+++ Assets/Scripts/ToLevel1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Switches player to level 1
+/// </summary>
+public class ToLevel1 : MonoBehaviour
+{
+    /// <summary>
+    /// updates every frame. on mouse down, shoots a ray out to see where the mouse is pointed and moves the character there
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ToTac.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Switches scenes to Tac
+/// </summary>
+public class ToTac : MonoBehaviour
+{
+    /// <summary>
+    /// updates every frame. on mouse down, shoots a ray out to see where the mouse is pointed and moves the character there
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                SceneManager.LoadScene("Tac", LoadSceneMode.Single);
+            }
+        }
+    }
+}
+
