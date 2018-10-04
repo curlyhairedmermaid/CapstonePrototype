e81d2e238d2c996a1dc7256bc9ea9b3bde96f435
Unknown <petroej1@ferris.edu>
2018-10-03 21:56:16 -0400

    Added overworld
    +overworld
    +playing around with AI

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    /// <summary>
+    /// The agent being called
+    /// </summary>
+    UnityEngine.AI.NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    /// <summary>
+    /// updates every frame. on mouse down, shoots a ray out to see where the mouse is pointed and moves the character there
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
